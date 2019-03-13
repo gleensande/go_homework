af914c453f6535ca60eef9f3d8a337468a6261d0
Irina Nikiforova <gleensande@gmail.com>
2019-03-13 17:20:11 +0300

    calc created

--- homework/calc/calc.go
+++ homework/calc/calc.go
@@ -1,4 +1,79 @@
 package main
 
-// сюда писать код
-// фукция main тоже будет тут
+import (
+	"os"
+	"io"
+	"bufio"
+	"strconv"
+	"strings"
+	"errors"
+)
+
+func main() {
+	out := os.Stdout
+	in := os.Stdin
+
+	scanner := bufio.NewScanner(in)
+
+	for scanner.Scan() {
+		calc(scanner.Text(), out)
+	}
+}
+
+func calc(in string, out io.Writer) error {
+	ins := strings.Split(in, " ")
+	var stack [1000]int
+	var err error
+
+	sp := 0
+
+	for _, r := range ins {
+		var x int
+
+		if sp < 0 {
+			return errors.New("abnormal syntax")
+		}
+
+		switch r {
+			case " " : fallthrough
+			case "\n" :
+			case "=" :
+				break
+			case "+":
+				if sp < 2 {
+					return errors.New("abnormal syntax")
+				}
+				stack[sp - 2] = stack[sp - 2] + stack[sp - 1]
+				sp--
+			case "-":
+				if sp < 2 {
+					return errors.New("abnormal syntax")
+				}
+				stack[sp - 2] = stack[sp - 2] - stack[sp - 1]
+				sp--
+			case "*":
+				if sp < 2 {
+					return errors.New("abnormal syntax")
+				}
+				stack[sp - 2] = stack[sp - 1] * stack[sp - 2]
+				sp--
+			case "/":
+				if sp < 2 {
+					return errors.New("abnormal syntax")
+				}
+				stack[sp - 2] = stack[sp - 2] / stack[sp - 1]
+				sp--
+			default:
+				x, err = strconv.Atoi(r)
+				if err != nil {
+					return err
+				}
+				stack[sp] = x
+				sp++
+		}
+	}
+
+	out.Write([]byte(strconv.Itoa(stack[sp - 1])))
+
+	return err
+}
--- homework/calc/calc_test.go
+++ homework/calc/calc_test.go
@@ -1,3 +1,56 @@
 package main
 
-// сюда писать тесты
+import (
+	"bytes"
+	"testing"
+)
+
+const testSimpleString = `1 2 3 4 + * + =`
+const testSimpleResult = `15`
+
+func TestCalcSimple(t *testing.T) {
+	out := new(bytes.Buffer)
+	err := calc(testSimpleString, out)
+	if err != nil {
+		t.Errorf("test for OK Failed - error")
+	}
+	result := out.String()
+	if result != testSimpleResult {
+		t.Errorf("test for OK Failed - results not match\nGot:\n%v\nExpected:\n%v", result, testSimpleResult)
+	}
+}
+
+const testSimpleString1 = `1 2 + 3 4 + * =`
+const testSimpleResult1 = `21`
+
+func TestCalcSimple1(t *testing.T) {
+	out := new(bytes.Buffer)
+	err := calc(testSimpleString1, out)
+	if err != nil {
+		t.Errorf("test for OK Failed - error")
+	}
+	result := out.String()
+	if result != testSimpleResult1 {
+		t.Errorf("test for OK Failed - results not match\nGot:\n%v\nExpected:\n%v", result, testSimpleResult1)
+	}
+}
+
+const testAbnormalString = `1 2 3 * * * * = = = =`
+const testAbnormalResult = ``
+const testAbnormalError = `abnormal syntax`
+
+func TestCalcAbnormanl(t *testing.T) {
+	out := new(bytes.Buffer)
+	err := calc(testAbnormalString, out)
+	if err != nil {
+		if err.Error() != testAbnormalError	{
+			t.Errorf("test for OK Failed - errors not match\nGot:\n%v\nExpected:\n%v", err.Error(), testAbnormalError)
+		}
+	}
+	result := out.String()
+	if result != testAbnormalResult {
+		t.Errorf("test for OK Failed - results not match\nGot:\n%v\nExpected:\n%v", result, testAbnormalResult)
+	}
+}
+
+
